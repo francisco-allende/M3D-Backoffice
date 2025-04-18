c73f4020c795593b22fe0411ac7877b9fa54ff2c
fallende <franciscoallende@lakaut.com.ar>
2025-04-18 13:22:27 -0300

    todo sigue igual

--- backoffice/m3d_app/models/suscriptor/particular_sin_impresora.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from .suscriptor import Suscriptor
-
-class ParticularSinImpresora(models.Model):
-    suscriptor = models.OneToOneField(Suscriptor, on_delete=models.CASCADE, related_name='particular_sin_impresora')
-
-    def __str__(self):
-        return f"{self.suscriptor.nombre} {self.suscriptor.apellido} (Sin impresora)"
--- backoffice/m3d_app/test.sql
+++
@@ -1,6 +0,0 @@
--- SQLite
-CREATE TABLE emp(empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL);
-
-insert into emp(empid, empname) values (1, 'pepe');
-
-select * from emp;
